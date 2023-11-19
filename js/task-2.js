5cc73ed7043ae4e301cd9a8b808b7da35fe42eb1
Igor <igorzovnodij19@gmail.com>
2023-11-19 18:07:02 +0200

    g

--- js/task-2.js
+++ js/task-2.js
@@ -6,7 +6,7 @@ let deliveryFee;
 
 function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
-    return "Shipping to <country> will cost <totalPrice> credits";
+    return 'Shipping to <country> will cost <totalPrice> credits';
 }
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
